087307b1f917aaf71f365c371ff5e0a1a020fd92
spongeyperson <spongeyperson1000@live.com>
2021-12-18 18:24:11 -0800

    Clone from spongeyperson/arch-dotfiles

---
+++ README.md
@@ -0,0 +1,39 @@
+# <p align=center>- Spongey's Arch Linux KDE Dotfiles -
+###### <p align=center> A Simple Git Repository to store various Arch Linux User Configs (Dotfiles).
+
+
+#### <p align=center> Links:
+  - Host Config: [spongeyperson/proxmox-config](https://github.com/spongeyperson/proxmox-config/)
+  - Unused Files Directory: [proxmox-config/home/tyler/arch-dotfiles/unused](https://github.com/spongeyperson/arch-dotfiles/tree/master/home/tyler/arch-dotfiles/unused)
+
+![I run Arch BTW](https://user-images.githubusercontent.com/28176188/140734509-d661b98b-b5bd-4ffc-9f59-69d9bed8f1c9.png)
+
+# Gaming Tweaks:
+
+## <img src="https://user-images.githubusercontent.com/28176188/142364090-9c9b1eaf-8e94-4402-b943-0d46895032f2.png" width="25" height="25"> Steam:
+###### Various Launch Commands used for Steam:
+
+<img src="https://user-images.githubusercontent.com/28176188/142365376-270d160f-33c3-4012-a3d9-541ab65bfdb6.png" width="20" height="20"> Radeon Specific:
+
+- RAD-V ACO Recompiler (Recommended): [^1]
+```bash
+VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json RADV_PERFTEST=aco suspend_compositing gamemoderun mangohud %command%
+```
+
+- AMDVLK: [^1]
+```bash
+VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/amd_icd64.json suspend_compositing gamemoderun mangohud %command%
+```
+
+<img src="https://user-images.githubusercontent.com/28176188/142362826-8090a147-94ee-4f67-a3ed-f87058a6797d.png" width="20" height="20"> Nvidia Specific:
+
+- Work In Progress
+
+**Please note:** `suspend_compositing` script sometimes prevents certain games with Anticheat or launchers from starting. If your game instantly closes, try removing that and suspending compositing with `Alt`+`Shift`+`F12` instead.
+
+## <img src="https://user-images.githubusercontent.com/28176188/142367009-ea2326c6-16ca-494a-9a4f-2591f90e2cae.png" width="25" height="25"> Lutris:
+
+- Work In Progress
+
+[^Note]: Unused Directory 
+[^1]: "[mangohud](https://github.com/spongeyperson/arch-config/blob/master/home/tyler/.config/MangoHud/MangoHud.conf)" and "[suspend_compositing](https://github.com/spongeyperson/arch-config/blob/master/usr/local/bin/suspend_compositing)" commands refer to the following config files required to make them work.
---
+++ home/tyler/.config/fish/config.fish
@@ -0,0 +1,49 @@
+#if status is-interactive
+#     # Commands to run in interactive sessions can go here
+#end
+
+# General Aliases
+alias tb="nc termbin.com 9999"
+alias termbin="nc termbin.com 9999"
+alias clbin="curl -F 'clbin=<-' https://clbin.com"
+alias aliases="vim /home/tyler/.config/fish/config.fish"
+alias fishconfig="vim /home/tyler/.config/fish/config.fish"
+alias vi="vim"
+alias phone="scrcpy -t --bit-rate 15M --max-fps 60 --window-title 'Samsung Galaxy S9+'"
+alias grep="grep --color=always"
+
+# File Management Tweaks
+alias mv="mv -v"
+alias cp="cp -v"
+alias ls="ls -Ch --color=always"
+alias l="ls -alh --color=always"
+alias ..="cd .."
+alias ...="cd -"
+alias chmod="chmod -v"
+alias chown="chown -v"
+
+# SSH
+alias sshkeys="echo -e '\033[0;32mAuthorized SSH Devices:\033[0m' && ls /home/tyler/.ssh/ssh-identities/ -1I '*.pub'"
+alias sshkey="echo -e '\033[0;32mAuthorized SSH Devices:\033[0m' && ls /home/tyler/.ssh/ssh-identities/ -1I '*.pub'"
+alias keys="echo -e '\033[0;32mAuthorized SSH Devices:\033[0m' && ls /home/tyler/.ssh/ssh-identities/ -1I '*.pub'"
+alias key="echo -e '\033[0;32mAuthorized SSH Devices:\033[0m' && ls /home/tyler/.ssh/ssh-identities/ -1I '*.pub'"
+
+
+# Manipulate KDE
+alias lockwidgets="echo -e '\033[0;31mWidgets Locked\033[0m' ; qdbus org.kde.plasmashell /PlasmaShell evaluateScript 'lockCorona(true)'"
+alias unlockwidgets="echo -e '\033[0;32mWidgets Unlocked\033[0m' ; qdbus org.kde.plasmashell /PlasmaShell evaluateScript 'lockCorona(false)'"
+alias restartkwin="killall kwin_x11 ; nohup kstart5 kwin_x11 </dev/null &>/dev/null & ; echo -e '\033[0;32mKwin Restarted'"
+alias restartplasma="killall plasmashell ; nohup kstart5 plasmashell </dev/null &>/dev/null & ; echo -e '\033[0;32mPlasma Shell Restarted'"
+alias restartdock="killall latte-dock ; nohup kstart5 latte-dock </dev/null &>/dev/null & ; echo -e '\033[0;32mLatte Dock Restarted'"
+
+# Dotfiles Sync
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=/'
+
+# Radeon Vulkan Settings
+alias radv="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/radeon_icd.x86_64.json'"
+alias radv64="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/radeon_icd.x86_64.json'"
+alias radv32="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/radeon_icd.i686.json'"
+alias amdvlk="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/amd_icd64.json'"
+alias amdvlk64="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/amd_icd64.json'"
+alias amdvlk32="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/amd_icd32.json'"
+##
